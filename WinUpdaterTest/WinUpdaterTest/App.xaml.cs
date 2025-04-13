795e5439542bfa1a01e3e928c506cd2593273f89
umai-beer <umai-beer@hotmail.co.jp>
2025-04-13 21:55:33 +0900

    ok

---
+++ WinUpdaterTest/WinUpdaterTest/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Windows;
+
+namespace WinUpdaterTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static IServiceProvider Services { get; protected set; } = ConfigureServices();
+
+
+        private static IServiceProvider ConfigureServices()
+        {
+            IServiceCollection? services = new ServiceCollection();
+            services.AddSingleton(provider =>
+            {
+                return new AppUpdater.AppUpdateService(
+                    appclose: () =>
+                    {
+                        // アプリケーションを終了する処理
+                        Application.Current.Shutdown();
+                    },
+                    appcastUrl: new Uri("https://github.com/okpnet/UpdaterTest/releases/download/0.0.1/appcast.xml"),
+                    publicKeyPath: new FileInfo("publickey.pem")
+                );
+            });
+            return services.BuildServiceProvider();
+        }
+    }
+
+}
