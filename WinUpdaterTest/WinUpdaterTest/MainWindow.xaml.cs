fb205554337c8dc65bb072f79740627fe252bec9
hayato <umai-beer@hotmail.co.jp>
2025-04-15 13:40:10 +0900

    ok

--- WinUpdaterTest/WinUpdaterTest/MainWindow.xaml.cs
+++ WinUpdaterTest/WinUpdaterTest/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using AppUpdater;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reactive.Disposables;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -16,8 +17,9 @@ namespace WinUpdaterTest
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window,IDisposable
     {
+        readonly CompositeDisposable disposables = new CompositeDisposable();
         readonly AppUpdateService _appUpdateService;
         public MainWindow()
         {
@@ -25,6 +27,17 @@ namespace WinUpdaterTest
             Resources["App"] = Application.Current;
 
             _appUpdateService = App.Services.GetRequiredService<AppUpdateService>();
+            disposables.Add(
+                _appUpdateService.UpdateStandbyEvent.
+                    Subscribe(t => t.IsCancel = true)
+                );
+            
+            _appUpdateService.UpdateDitectAsync();
+        }
+
+        public void Dispose()
+        {
+            disposables.Clear();
         }
     }
 }
