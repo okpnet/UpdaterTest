5abcca1b0ade3d86a256e3fce356313f2fa77857
hayato <umai-beer@hotmail.co.jp>
2025-04-15 16:25:10 +0900

    ok

--- WinUpdaterTest/WinUpdaterTest/MainWindow.xaml.cs
+++ WinUpdaterTest/WinUpdaterTest/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using AppUpdater;
 using Microsoft.Extensions.DependencyInjection;
+using System.ComponentModel;
 using System.Reactive.Disposables;
 using System.Text;
 using System.Windows;
@@ -17,27 +18,84 @@ namespace WinUpdaterTest
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window,IDisposable
+    public partial class MainWindow : Window, IDisposable,INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string PropertyName)
+        {
+            var e = new PropertyChangedEventArgs(PropertyName);
+            PropertyChanged?.Invoke(this, e);
+        }
+
         readonly CompositeDisposable disposables = new CompositeDisposable();
         readonly AppUpdateService _appUpdateService;
+
+        Action<bool> triggerAction=default!;
+
+        int _prg = 0;
+        public int Progress
+        {
+            get => _prg;
+            set
+            {
+                _prg = value;
+                NotifyPropertyChanged(nameof(Progress));
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
             Resources["App"] = Application.Current;
 
             _appUpdateService = App.Services.GetRequiredService<AppUpdateService>();
+
             disposables.Add(
-                _appUpdateService.UpdateStandbyEvent.
-                    Subscribe(t => t.IsCancel = true)
-                );
+                _appUpdateService.UpdaterDownloadProgressEvent.Subscribe(t =>
+                {
+                    Progress = t.ProgressPercent;
+                }));
+
+            disposables.Add(
+                _appUpdateService.UpdateStandbyEvent.Subscribe((t) =>
+                {
+                    t.ShouldWait = true;
+                    triggerAction = (isUpdate) => t.TriggerUpdate(isUpdate);
+                }));
             
-            _appUpdateService.UpdateDitectAsync();
         }
 
         public void Dispose()
         {
             disposables.Clear();
+        }
+
+        private void DitecedBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _appUpdateService.UpdateDitectAsync();
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CnacelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (triggerAction is not null)
+            {
+                triggerAction.Invoke(false);
+            }
+        }
+
+        private void NoeUpdaterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(triggerAction is not null)
+            {
+                triggerAction.Invoke(true);
+            }
         }
     }
 }
